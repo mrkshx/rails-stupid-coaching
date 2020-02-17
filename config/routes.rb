9256bf4f753972519dfc60301db9d0380d881142
Markus Mengel <markus.mengelhx@gmail.com>
2020-02-17 15:16:07 +0100

    question and answer working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get 'questions/ask', as: :ask
+  # get 'questions/answer', as: :answer
+
+  get '/ask', to: 'questions#ask', as: :ask
+  get '/answer', to: 'questions#answer', as: :answer
 end
